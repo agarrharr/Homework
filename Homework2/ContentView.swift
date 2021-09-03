e282782b0765a74f3ae2ffa221ebbf5501b48366
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-09-03 08:31:34 -0600

    Initial Commit

---
+++ Homework2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework2
+//
+//  Created by Adam Garrett-Harris on 9/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homework2/Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2App.swift
+//  Homework2
+//
+//  Created by Adam Garrett-Harris on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
